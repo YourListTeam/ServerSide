c3774982b9138ed700c88114b4503f87e55d207e
Artrur Azarskyy <artur.azarskyy@mail.utoronto.ca>
2020-02-03 11:43:22 -0500

    [YOUR-36] Application is connected to Firebase
    Application is not connected to firebase. Small note
    I cannot test whether iOS part actually works. I need someone to
    test it out. For instructions reffer to AppDeligate.swift or go to
    FireBase project and continue setup from step 3 but skip step 4.

--- YourListApp/ios/Runner/AppDelegate.swift
+++ YourListApp/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,11 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+# I dont know if this is comment in swift but
+#TODO: I need someone with mac to test this out, got to iOS folder and run 'pod install'
+# https://firebase.google.com/docs/ios/setup?authuser=0#frameworks
