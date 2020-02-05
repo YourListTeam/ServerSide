9d553024262246d072f28701a671ba52f22b7178
Krish <krish.chowdhary@utoronto.ca>
2020-02-05 14:51:05 -0500

    YOUR-34 added db demo

--- Serverside/routes/users.js
+++ Serverside/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var dbclient = require('../model/database.js');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({"userid":"1234",'username':"krishc"});
+  dbclient.query('select * from Users where username="scashin0"', (err, res) => {
+    res.json((err, res));
+  });
+  dbclient.end();
 });
 
 module.exports = router;
