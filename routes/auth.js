6bdaee38a4ae154862441f8f177411cd637e8f73
Anshu <anshu.pandeya@mail.utoronto.ca>
2020-02-13 20:33:44 -0500

    YOUR-12 endpoints for list creation and authorization is localized

---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var dbclient = require('../model/database.js');
+
+/* GET auth listing. */
+router.get('/', function(req, res, next) {
+    if (("UUID" in req.body) && ("LID" in req.body)) {
+      dbclient.get_auth(res, req.body["UUID"],req.body["LID"])
+    }
+    else {
+      res.status(400);
+    }
+  });
+
+/* POST created auth. */
+router.post('/', function(req, res, next) {
+  if (("UUID" in req.body) && ("LID" in req.body)) {
+    dbclient.create_auth(res,req.body["UUID"], req.body["LID"])
+  } else {
+    res.status(400);
+  }
+});
+
+  module.exports = router;
