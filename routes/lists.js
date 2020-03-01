510b6b21373d81339ff10e23e37d85cc88f10fe2
Krish Chowdhary <krish.chowdhary@utoronto.ca>
2020-03-01 14:45:39 -0500

    refactoring

--- routes/lists.js
+++ routes/lists.js
@@ -1,37 +1,76 @@
 const express = require('express');
+const uuidGen = require('uuid');
 const dbclient = require('../model/database.js');
 
 const router = express.Router();
+
 /* GET list listing. */
-router.get('/', (req, res) => {
-    if ('LID' in req.body) {
-        dbclient.get_list(res, req.body.LID);
+async function getHandler(body) {
+    const output = {};
+    if ('LID' in body) {
+        const ret = await dbclient.get_list(body.LID);
+        if (ret.rows) {
+            output.status = 200;
+            [output.json] = [ret.rows[0]];
+        } else {
+            output.status = 404;
+        }
     } else {
-        res.status(400).end();
+        output.status = 400;
+    }
+    return output;
+}
+
+router.get('/', async (req, res) => {
+    const output = await getHandler(req.body);
+    if ('json' in output) {
+        res.status(output.status).json(output.json);
+    } else {
+        res.status(output.status).end();
     }
 });
 
 /* POST list. */
-router.post('/', (req, res) => {
-    if ('LID' in req.body) {
-        dbclient.create_new_list(res, req.body.UUID, req.body.LID,
-            req.body.listname, req.body.Colour);
+async function postHandler(body) {
+    const output = {};
+    if ('UUID' in body) {
+        const lid = uuidGen.v4();
+        await dbclient.create_new_list(lid, body.listname, body.Colour);
+        await dbclient.add_user(body.UUID, lid, 15);
+        output.status = 200;
     } else {
-        res.status(400).end();
+        output.status = 400;
     }
+    return output;
+}
+
+router.post('/', async (req, res) => {
+    const output = await postHandler(req.body);
+    res.status(output.status).end();
 });
 
-router.get('/readable_lists', async (req, res) => {
-    if ('UUID' in req.body) {
-        const perms = await dbclient.readable_lists(req.body.UUID);
+async function getReadableHandler(body) {
+    const output = {};
+    if ('UUID' in body) {
+        const perms = await dbclient.readable_lists(body.UUID);
         const r = [];
         for (let i = 0; i < perms.rows.length; i += 1) {
             r.push(perms.rows[i].lid);
         }
-        res.json(r);
-        res.status(200);
+        output.json = r;
+        output.status = 200;
     } else {
-        res.status(400);
+        output.status = 400;
+    }
+    return output;
+}
+
+router.get('/readable_lists', async (req, res) => {
+    const output = await getReadableHandler(req.body);
+    if ('json' in output) {
+        res.status(output.status).json(output.json);
+    } else {
+        res.status(output.status).end();
     }
 });
 
