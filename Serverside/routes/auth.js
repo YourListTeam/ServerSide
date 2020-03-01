0a67510db980f5f3fd12a8843c81c6c327472aea
Anshu <anshu.pandeya@mail.utoronto.ca>
2020-03-01 10:23:15 -0500

    YOUR-15 linting code

--- Serverside/routes/auth.js
+++ Serverside/routes/auth.js
@@ -1,26 +1,26 @@
-var express = require('express');
-var router = express.Router();
-var dbclient = require('../model/database.js');
+const express = require('express');
+
+const router = express.Router();
+const dbclient = require('../model/database.js');
 
 /* GET auth listing. */
-router.get('/', async function(req, res, next) {
-    if (("UUID" in req.body) && ("LID" in req.body)) {
-        result = await dbclient.authenticate_list(req.body["LID"], req.body["UUID"]);
+router.get('/', async (req, res, next) => {
+    if (('UUID' in req.body) && ('LID' in req.body)) {
+        result = await dbclient.authenticate_list(req.body.LID, req.body.UUID);
         if (result) {
             res.json(result[0]);
         } else {
             res.status(404).end();
         }
-    }
-    else {
-      res.status(400).end();
+    } else {
+        res.status(400).end();
     }
 });
 
 /* POST auth for a given list and user. */
-router.post('/admin', async function(req, res, next) {
-    if (("UUID" in req.body) && ("LID" in req.body)) {
-        result = await dbclient.create_admin(req.body["UUID"], req.body["LID"]);
+router.post('/admin', async (req, res, next) => {
+    if (('UUID' in req.body) && ('LID' in req.body)) {
+        result = await dbclient.create_admin(req.body.UUID, req.body.LID);
         if (result.rows) {
             res.status(200).end();
         } else {
@@ -32,15 +32,15 @@ router.post('/admin', async function(req, res, next) {
 });
 
 /* DELETE another user for a given list. */
-router.delete('/user', async function(req, res, next) {
-    if ("LID" in req.body && "UUID" in req.body && 'OUUID' in req.body) {
+router.delete('/user', async (req, res, next) => {
+    if ('LID' in req.body && 'UUID' in req.body && 'OUUID' in req.body) {
         // user needs to have admin or modify permissions
-        user_permission = await dbclient.authenticate_list(req.body['LID'],req.body['UUID']);
-        
-        if (dbclient.is_admin(user_permission)) {
-            contact_permission = await dbclient.user_in_list(req.body['OUUID'],req.body['LID']);
-            if (contact_permission.rows[0]){
-                result = await dbclient.delete_contact(req.body['OUUID'],req.body['LID']);
+        const userPermission = await dbclient.authenticate_list(req.body.LID, req.body.UUID);
+
+        if (dbclient.is_admin(userPermission)) {
+            const contactPermission = await dbclient.user_in_list(req.body.OUUID, req.body.LID);
+            if (contactPermission.rows[0]) {
+                result = await dbclient.delete_contact(req.body.OUUID, req.body.LID);
                 res.status(200).end();
             } else {
                 res.status(400).end();
