5115ecd1ab1bf558086f6ab01b371015ddacf141
Krish Chowdhary <krish.chowdhary@utoronto.ca>
2020-03-03 10:43:37 -0500

    small fix

--- Serverside/deploy.sh
+++ Serverside/deploy.sh
@@ -11,9 +11,21 @@ chmod 644 ~/.ssh/id_rsa.pub
 echo "${PRIVATE_KEY}" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
+export PUBLIC_KEY= "";
+export PRIVATE_KEY= "";
+
 # Preload the known_hosts file  (see "version 2" below)
 echo '|1|bylPsc3uS1RQlpazXkm7TWGJOmI=|eEufAkxxshMXHoKRx79WhiSgm1E= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDbI2skbGdMXwfidMPoVneQsH5DUbkvZ9sJxtzfanuEpanQShDYym9NHsuQB/bjuUnE20eQAW6H7k969bC0nrmU=
 |1|m9Kx3Dd7szntl/5k9vkRB9M6K/c=|bR2IchOokbRroSyxyM62Y5JEmQk= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDbI2skbGdMXwfidMPoVneQsH5DUbkvZ9sJxtzfanuEpanQShDYym9NHsuQB/bjuUnE20eQAW6H7k969bC0nrmU=' > ~/.ssh/known_hosts
+
+echo "Host cslinux
+    Hostname cslinux.utm.utoronto.com
+    Port 22
+    User {$UTORID}
+    ServerAliveInterval 240
+    ServerAliveCountMax 2" > ~/.ssh/config
+
+
 # Start the SSH tunnel if not already running
 
 PID=`pgrep -f ssh`
