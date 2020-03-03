20ffa3ebbdbf228f27484f892005defd4f496239
Krish Chowdhary <krish.chowdhary@utoronto.ca>
2020-03-03 00:48:22 -0500

    updated

--- Serverside/setup.sh
+++ Serverside/setup.sh
@@ -12,7 +12,8 @@ echo "${PRIVATE_KEY}" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 # Preload the known_hosts file  (see "version 2" below)
-
+echo '|1|bylPsc3uS1RQlpazXkm7TWGJOmI=|eEufAkxxshMXHoKRx79WhiSgm1E= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDbI2skbGdMXwfidMPoVneQsH5DUbkvZ9sJxtzfanuEpanQShDYym9NHsuQB/bjuUnE20eQAW6H7k969bC0nrmU=
+|1|m9Kx3Dd7szntl/5k9vkRB9M6K/c=|bR2IchOokbRroSyxyM62Y5JEmQk= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDbI2skbGdMXwfidMPoVneQsH5DUbkvZ9sJxtzfanuEpanQShDYym9NHsuQB/bjuUnE20eQAW6H7k969bC0nrmU=' > ~/.ssh/known_hosts
 # Start the SSH tunnel if not already running
 
 PID=`pgrep -f ssh`
