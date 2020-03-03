c3c5a0f0699486f375f8db517205e51907ea0f4b
Krish Chowdhary <krish.chowdhary@utoronto.ca>
2020-03-03 00:31:23 -0500

    updated script

--- Serverside/setup.sh
+++ Serverside/setup.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
+echo "creating public and private key files";
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
 echo $PRIVATE_KEY > id_rsa;
 echo $PUBLIC_KEY > id_rsa.pub;
-ssh -i id_rsa -f -N -L 5433:mcsdb.utm.utoronto.ca:5432 cslinux.utm.utoronto.ca;
+
+# Create the public and private key files from the environment variables.
+echo "${PUBLIC_KEY}" > ~/.ssh/id_rsa.pub
+chmod 644 $~/.ssh/heroku_id_rsa.pub
+
+# Note use of double quotes, required to preserve newlines
+echo "${PRIVATE_KEY}" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/heroku_id_rsa
+
+# Preload the known_hosts file  (see "version 2" below)
+
+# Start the SSH tunnel if not already running
+
+PID=`pgrep -f ssh`
+if [ $PID ] ; then
+    echo "tunnel already running on ${PID}";
+else
+    echo "launching tunnel";
+    ssh -f -i ~/.ssh/id_rsa -N -L 5433:mcsdb.utm.utoronto.ca:5432 $UTORID@cslinux.utm.utoronto.ca;
+fi
+
 node app.js;
