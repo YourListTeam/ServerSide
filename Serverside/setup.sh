b8905fc9b5b96974c1d6f8c36d69f63011594d21
Krish Chowdhary <krish.chowdhary@utoronto.ca>
2020-03-03 00:19:24 -0500

    fix

--- Serverside/setup.sh
+++ Serverside/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo $PRIVATE_KEY > ~/.ssh/id_rsa;
-echo $PUBLIC_KEY > ~/.ssh/id_rsa.pub;
-ssh -f -N -L 5433:mcsdb.utm.utoronto.ca:5432 @cslinux.utm.utoronto.ca;
+echo $PRIVATE_KEY > id_rsa;
+echo $PUBLIC_KEY > id_rsa.pub;
+ssh -i id_rsa -f -N -L 5433:mcsdb.utm.utoronto.ca:5432 @cslinux.utm.utoronto.ca;
 node app.js;
