aa70d1c399b56effad1a3a43e9b47b49726992df
Artrur Azarskyy <artur.azarskyy@mail.utoronto.ca>
2020-03-04 14:13:38 -0500

    YOUR-40 Refactored codebase to use block pattern
    App now uses block patter for authentication. Only one instance
    of auth service is available and states are managed more carefully.

--- YourListApp/lib/main.dart
+++ YourListApp/lib/main.dart
@@ -1,38 +1,59 @@
-import 'package:your_list_flutter_app/screens/wrapper.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:your_list_flutter_app/screens/home/home.dart';
+import 'package:your_list_flutter_app/screens/splash_screen.dart';
 import 'package:your_list_flutter_app/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:your_list_flutter_app/models/user.dart';
 import 'models/userService.dart';
+import 'package:bloc/bloc.dart';
+import 'simple_bloc_delegate.dart';
+import 'authentication_block/authentication_bloc.dart';
+import 'screens/authenticate/authenticate.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final AuthService authService = AuthService();
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  runApp(
+    BlocProvider(
+        create: (context) => AuthenticationBloc(
+            authService: authService
+        )..add(AppStarted()),
+        child:MyApp(authService: authService),
+    )
+);
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final AuthService _authService;
+  MyApp({Key key, @required AuthService authService})
+      : assert(authService != null),
+        _authService = authService,
+        super(key: key);
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        StreamProvider<User>.value(value: AuthService().user),
         Provider(
             create: (_) => UserService.create(),
             dispose: (context, UserService service) => service.client.dispose(),
-        ),
+        ), // UserApi
       ],
       child: MaterialApp(
-        home: Wrapper(),
+        home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+            builder: (context, state) {
+              if(state is Unauthenticated){
+                return Authenticate(authService: _authService);
+              }
+              if (state is Authenticated){
+                return Home();
+              }
+              return SplashScreen();
+            }
+        ),
       ),
     );
 
-//    return StreamProvider<User>.value(
-//      value: AuthService().user,
-//      child: Provider(
-//        create: (_) => UserService.create(),
-//        dispose: (context, UserService service) => service.client.dispose(),
-//        child: MaterialApp(
-//          home: Wrapper(),
-//        ),
-//      ),
-//    );
   }
 }
--- YourListApp/lib/services/auth.dart
+++ YourListApp/lib/services/auth.dart
@@ -1,68 +1,52 @@
-import 'package:your_list_flutter_app/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class AuthService {
+class AuthService {final FirebaseAuth _firebaseAuth;
+final GoogleSignIn _googleSignIn;
 
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+AuthService({FirebaseAuth firebaseAuth, GoogleSignIn googleSignin})
+    : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+      _googleSignIn = googleSignin ?? GoogleSignIn();
 
-  // create user obj based on firebase user
-  User _userFromFirebaseUser(FirebaseUser user) {
-    return user != null ? User(uid: user.uid) : null;
-  }
+Future<FirebaseUser> signInWithGoogle() async {
+  final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+  final GoogleSignInAuthentication googleAuth =
+  await googleUser.authentication;
+  final AuthCredential credential = GoogleAuthProvider.getCredential(
+    accessToken: googleAuth.accessToken,
+    idToken: googleAuth.idToken,
+  );
+  await _firebaseAuth.signInWithCredential(credential);
+  return _firebaseAuth.currentUser();
+}
 
-  // auth change user stream
-  Stream<User> get user {
-    return _auth.onAuthStateChanged
-      //.map((FirebaseUser user) => _userFromFirebaseUser(user));
-      .map(_userFromFirebaseUser);
-  }
+Future<void> signInWithCredentials(String email, String password) {
+  return _firebaseAuth.signInWithEmailAndPassword(
+    email: email,
+    password: password,
+  );
+}
 
-  // sign in anon
-  Future signInAnon() async {
-    try {
-      AuthResult result = await _auth.signInAnonymously();
-      FirebaseUser user = result.user;
-      return _userFromFirebaseUser(user);
-    } catch (e) {
-      print(e.toString());
-      return null;
-    }
-  }
+Future<void> signUp({String email, String password}) async {
+  return await _firebaseAuth.createUserWithEmailAndPassword(
+    email: email,
+    password: password,
+  );
+}
 
-  // sign in with email and password
-  Future signInWithEmailAndPassword(String email, String password) async {
-    try {
-      AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
-      FirebaseUser user = result.user;
-      return user;
-    } catch (error) {
-      print(error.toString());
-      return null;
-    } 
-  }
+Future<void> signOut() async {
+  return Future.wait([
+    _firebaseAuth.signOut(),
+    _googleSignIn.signOut(),
+  ]);
+}
 
-  // register with email and password
-  Future registerWithEmailAndPassword(String email, String password) async {
-    try {
-      AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-      FirebaseUser user = result.user;
-//      print("This is sparta");
-//      print(user.uid);
-      return _userFromFirebaseUser(user);
-    } catch (error) {
-      print(error.toString());
-      return null;
-    } 
-  }
+Future<bool> isSignedIn() async {
+  final currentUser = await _firebaseAuth.currentUser();
+  return currentUser != null;
+}
 
-  // sign out
-  Future signOut() async {
-    try {
-      return await _auth.signOut();
-    } catch (error) {
-      print(error.toString());
-      return null;
-    }
-  }
-
+Future<String> getUser() async {
+  return (await _firebaseAuth.currentUser()).email;
+}
 }
