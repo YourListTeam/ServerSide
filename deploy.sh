8aa3f04257d7fcd460172f61d3c09220ef00ad1a
Krish Chowdhary <krish.chowdhary@utoronto.ca>
2020-03-03 21:14:03 -0500

    fixed

--- deploy.sh
+++ deploy.sh
@@ -18,12 +18,8 @@ export PRIVATE_KEY="";
 echo '|1|bylPsc3uS1RQlpazXkm7TWGJOmI=|eEufAkxxshMXHoKRx79WhiSgm1E= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDbI2skbGdMXwfidMPoVneQsH5DUbkvZ9sJxtzfanuEpanQShDYym9NHsuQB/bjuUnE20eQAW6H7k969bC0nrmU=
 |1|m9Kx3Dd7szntl/5k9vkRB9M6K/c=|bR2IchOokbRroSyxyM62Y5JEmQk= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDbI2skbGdMXwfidMPoVneQsH5DUbkvZ9sJxtzfanuEpanQShDYym9NHsuQB/bjuUnE20eQAW6H7k969bC0nrmU=' > ~/.ssh/known_hosts
 
-echo "Host cslinux
-    Hostname cslinux.utm.utoronto.com
-    Port 22
-    User $UTORID
-    ServerAliveInterval 240
-    ServerAliveCountMax 2" > ~/.ssh/config
+echo "Host *
+    ServerAliveInterval 240" > ~/.ssh/config
 
 
 # Start the SSH tunnel if not already running
