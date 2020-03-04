11a55318f270926f0e13cbb834ad8c220720e0d2
Krish Chowdhary <krish.chowdhary@utoronto.ca>
2020-03-04 14:51:48 -0500

    updated dummy data

--- model/sql_scripts/schema.sql
+++ model/sql_scripts/schema.sql
@@ -8,8 +8,8 @@ CREATE TABLE Users (
 );
 CREATE TABLE Lists (
     LID varchar(36) PRIMARY KEY,
-    listname varchar(10),
-    Colour varchar(11),
+    listname varchar(32),
+    Colour varchar(16),
     Modified date
 );
 CREATE TABLE Auth (
@@ -25,23 +25,24 @@ CREATE TABLE Items (
     IID varchar(36) PRIMARY KEY,
     UUID varchar(36),
     LID varchar(36),
-    Name varchar(10),
+    Name varchar(32),
     Completed boolean,
     Modified date,
-    FOREIGN KEY (LID) REFERENCES Lists(LID)
+    FOREIGN KEY (LID) REFERENCES Lists(LID) ON DELETE CASCADE,
+    FOREIGN KEY (UUID) REFERENCES Users(UUID) ON DELETE CASCADE
 );
 
 CREATE TABLE Favorites (
     UUID varchar(36),
     LID varchar(36),
-    FOREIGN KEY (UUID) REFERENCES Users(UUID),
-    FOREIGN KEY (LID) REFERENCES Lists(LID)
+    FOREIGN KEY (UUID) REFERENCES Users(UUID) ON DELETE CASCADE,
+    FOREIGN KEY (LID) REFERENCES Lists(LID) ON DELETE CASCADE
 );
 CREATE TABLE Contacts (
     User_UUID varchar(36),
     Contact_UUID varchar(36),
-    FOREIGN KEY (User_UUID) REFERENCES Users(UUID),
-    FOREIGN KEY (Contact_UUID) REFERENCES Users(UUID)
+    FOREIGN KEY (User_UUID) REFERENCES Users(UUID) ON DELETE CASCADE,
+    FOREIGN KEY (Contact_UUID) REFERENCES Users(UUID) ON DELETE CASCADE
 );
 
 CREATE TABLE Locations(
