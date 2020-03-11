ea1a9996ddbe5dc3bdfecac4ecf1da207b3e5c06
Krish Chowdhary <krish.chowdhary@utoronto.ca>
2020-03-11 14:21:04 -0400

    deploying

--- Serverside/deploy.sh
+++ Serverside/deploy.sh
@@ -15,11 +15,11 @@ export PUBLIC_KEY="";
 export PRIVATE_KEY="";
 
 # Preload the known_hosts file  (see "version 2" below)
-echo '|1|bylPsc3uS1RQlpazXkm7TWGJOmI=|eEufAkxxshMXHoKRx79WhiSgm1E= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDbI2skbGdMXwfidMPoVneQsH5DUbkvZ9sJxtzfanuEpanQShDYym9NHsuQB/bjuUnE20eQAW6H7k969bC0nrmU=
-|1|m9Kx3Dd7szntl/5k9vkRB9M6K/c=|bR2IchOokbRroSyxyM62Y5JEmQk= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDbI2skbGdMXwfidMPoVneQsH5DUbkvZ9sJxtzfanuEpanQShDYym9NHsuQB/bjuUnE20eQAW6H7k969bC0nrmU=' > ~/.ssh/known_hosts
+echo '|1|CKLNmnXUo20IblyvVtK5eqfxkhE=|S+SYmJgNXZiqCIJWGAnPKyb8bJA= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOqyHh/myL33e9GtA45bPkjaUEoO3nRqqsMgYaRB3ihuvAkiSQLZCMp5BbE8Dq4abL3XhfsdM21YDWxrHHS02Ow=
+|1|fymPIWUfoi/iq9PZP8jCC30IWGg=|Tjs/hHryX7xHP4rFcneRMf4CyeE= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOqyHh/myL33e9GtA45bPkjaUEoO3nRqqsMgYaRB3ihuvAkiSQLZCMp5BbE8Dq4abL3XhfsdM21YDWxrHHS02Ow=' > ~/.ssh/known_hosts
 
 echo "Host *
-    ServerAliveInterval 180" > ~/.ssh/config
+    ServerAliveInterval 120" > ~/.ssh/config
 
 
 # Start the SSH tunnel if not already running
@@ -29,7 +29,7 @@ if [ $PID ] ; then
     echo "tunnel already running on ${PID}";
 else
     echo "launching tunnel";
-    ssh -f -i ~/.ssh/id_rsa -N -L 5433:mcsdb.utm.utoronto.ca:5432 $UTORID@cslinux.utm.utoronto.ca;
+    ssh -f -i ~/.ssh/id_rsa -N -L 5433:mcsdb.utm.utoronto.ca:5432 $UTORID@dh2026pc00.utm.utoronto.ca;
 fi
 
 node app.js;
