70256b3487db4c89b24e58e05539d2b0486c76f2
Anshu <anshu.pandeya@mail.utoronto.ca>
2020-03-16 17:44:02 -0400

    YOUR-23 cleaning up code and fixing return statuses

--- routes/location.js
+++ routes/location.js
@@ -1,58 +1,55 @@
 const express = require('express');
 const dbclient = require('../model/database.js');
 const mbxGeocoding = require('../../node_modules/@mapbox/mapbox-sdk/services/geocoding');
+
 const MAPBOX_ACCESS_TOKEN = process.env.mapbox;
-const geocodingClient = mbxGeocoding({accessToken: MAPBOX_ACCESS_TOKEN});
+const geocodingClient = mbxGeocoding({ accessToken: MAPBOX_ACCESS_TOKEN });
 
 const router = express.Router();
 
 async function getLocationHandler(body) {
     // query to mapbox
-    const toSearch=body.Address;
     const promise = geocodingClient.forwardGeocode({
-        query: toSearch,
+        query: body.Address,
         countries: ['ca'],
-        limit: 3
-      })
+        limit: 3,
+    })
         .send()
-        .then(response => {
-          const match = response.body;
-          return match;
+        .then((response) => {
+            const match = response.body;
+            return match;
         }).catch(() => null);
-      return promise;
+    return promise;
 }
 
 /* POST location. */
 async function postLocationHandler(body) {
     const output = {};
-    const forwardencoding={};
+    output.status = 400;
     if ('LID' in body && 'UUID' in body && 'Address' in body) {
         // check if user has writing permission
-        output.status = 400;
+
         const permission = await dbclient.authenticate_list(body.LID, body.UUID);
         if (dbclient.can_write(permission)) {
-
-          // forward encode given address mapbox to get its coordinates
-          const promise = getLocationHandler(body).then((match) => {
-
+            // forward encode given address mapbox to get its coordinates
+            const getpromise = getLocationHandler(body).then((match) => {
             // check the result's relevance from query
-            if (match.features[0].relevance >= 0.8){
-              var long = match.features[0].geometry.coordinates[0];
-              var lat = match.features[0].geometry.coordinates[1];
+                if (match.features[0].relevance >= 0.8) {
+                    const long = match.features[0].geometry.coordinates[0];
+                    const lat = match.features[0].geometry.coordinates[1];
 
-              console.log(body.Address);
-              console.log(match.features[0].geometry.coordinates);
-
-              // add location to the table
-              dbclient.create_location(body.LID,long,lat);
-              output.status = 200;
-            }
-          }).catch(() => null);
-
-
-      }
-    } else {
-        output.status = 400;
+                    // add location to the table
+                    const creationpromise = dbclient.create_location(body.LID, long,
+                        lat).then(() => {
+                        output.status = 200;
+                        return output;
+                    }).catch(() => null);
+                    return creationpromise;
+                }
+                return output;
+            }).catch(() => null);
+            return getpromise;
+        }
     }
     return output;
 }
