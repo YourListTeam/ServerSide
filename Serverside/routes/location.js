3673c20b5c9451ea933dfc4fb855ece791c399e5
Anshu <anshu.pandeya@mail.utoronto.ca>
2020-03-16 16:47:25 -0400

    YOUR-23 post function uses promises instead of callbacks

--- Serverside/routes/location.js
+++ Serverside/routes/location.js
@@ -7,9 +7,9 @@ const geocodingClient = mbxGeocoding({accessToken: MAPBOX_ACCESS_TOKEN});
 const router = express.Router();
 
 async function getLocationHandler(body) {
-  output={};
-    var toSearch=body.Address;
-    geocodingClient.forwardGeocode({
+    // query to mapbox
+    const toSearch=body.Address;
+    const promise = geocodingClient.forwardGeocode({
         query: toSearch,
         countries: ['ca'],
         limit: 3
@@ -17,17 +17,13 @@ async function getLocationHandler(body) {
         .send()
         .then(response => {
           const match = response.body;
-          // console.log(match.features[0].relevance);
-          // console.log(match.features[0].geometry.coordinates);
-          output.relevance = match.features[0].relevance;
-          output.coordinates=match.features[0].geometry.coordinates;
-          output.status=200;
-          return output;
-        });
+          return match;
+        }).catch(() => null);
+      return promise;
 }
 
 /* POST location. */
-async function postLocationHandler(body,callback) {
+async function postLocationHandler(body) {
     const output = {};
     const forwardencoding={};
     if ('LID' in body && 'UUID' in body && 'Address' in body) {
@@ -35,27 +31,25 @@ async function postLocationHandler(body,callback) {
         output.status = 400;
         const permission = await dbclient.authenticate_list(body.LID, body.UUID);
         if (dbclient.can_write(permission)) {
-          console.log(body.Address);
+
           // forward encode given address mapbox to get its coordinates
-          var temp = await callback(body);
-          console.log(temp);
+          const promise = getLocationHandler(body).then((match) => {
 
-          // check the result's relevance from query
-          if (forwardencoding.rel >= 0.8){
-            var long = forwardencoding.coo[0];
-            var lat = forwardencoding.coo[1];
-            console.log(long);
-            console.log(lat);
+            // check the result's relevance from query
+            if (match.features[0].relevance >= 0.8){
+              var long = match.features[0].geometry.coordinates[0];
+              var lat = match.features[0].geometry.coordinates[1];
 
-            // must check if the location already exists in the table
-            const locExists = await dbclient.check_location(body.LID,
-            long, lat);
-            console.log(locExists);
+              console.log(body.Address);
+              console.log(match.features[0].geometry.coordinates);
 
-            // await dbclient.create_location(body.LID,long,lat);
-            output.status = 200;
-          }
-            
+              // add location to the table
+              dbclient.create_location(body.LID,long,lat);
+              output.status = 200;
+            }
+          }).catch(() => null);
+
+
       }
     } else {
         output.status = 400;
@@ -64,7 +58,7 @@ async function postLocationHandler(body,callback) {
 }
 
 router.post('/', async (req, res) => {
-    const output = await postLocationHandler(req.body,getLocationHandler);
+    const output = await postLocationHandler(req.body);
     res.status(output.status).end();
 });
 
